3dbe4d83b284d29c3e4973ade9712a7a1a178871
ClawNarok <arthurvgf@gmail.com>
2022-11-19 19:30:50 -0300

    fix lint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path='/'
-            element={<PatientsList/>}
+            path="/"
+            element={ <PatientsList /> }
             exact
           />
         </Routes>
