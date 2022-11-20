aa9dee5704db2d0ef4e4c4a41608d8b0a0fb147c
ClawNarok <arthurvgf@gmail.com>
2022-11-20 06:03:26 -0300

    update PatientsList.jsx

--- frontend/src/Pages/PatientsList.jsx
+++ frontend/src/Pages/PatientsList.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import CCreateButton from '../Components/CCreateButton';
 
 function PatientsList() {
+  const navigate = useNavigate();
+
   return (
-    <h1>Patients</h1>
+    <>
+      <h1>Patients</h1>
+      <CCreateButton
+        txtButton="Adicionar paciente"
+        callback={ () => navigate('/cadastro') }
+      />
+    </>
   );
 }
 
