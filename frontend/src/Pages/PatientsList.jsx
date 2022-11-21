10e38da24bac90af70526a91a46878967f60cf56
ClawNarok <arthurvgf@gmail.com>
2022-11-21 00:45:53 -0300

    update PatientsList.jsx

--- frontend/src/Pages/PatientsList.jsx
+++ frontend/src/Pages/PatientsList.jsx
@@ -1,18 +1,21 @@
+import { Container } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import CCreateButton from '../Components/CCreateButton';
+import CDataGrid from '../Components/CDataGrid';
 
 function PatientsList() {
   const navigate = useNavigate();
 
   return (
-    <>
+    <Container>
       <h1>Patients</h1>
       <CCreateButton
         txtButton="Adicionar paciente"
         callback={ () => navigate('/cadastro') }
       />
-    </>
+      <CDataGrid />
+    </Container>
   );
 }
 
