bd94da6e28e53714d54d48fa6b88defe0d068f20
ClawNarok <arthurvgf@gmail.com>
2022-11-27 23:25:19 -0300

    update PatientsProvider.js

--- frontend/src/Context/PatientsProvider.js
+++ frontend/src/Context/PatientsProvider.js
@@ -1,17 +1,56 @@
 import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import PatientsContext from './PatientsContext';
+import {
+  getAllPatients,
+  getPatientById,
+  addPatient,
+  updatePatientById,
+  deletePatientById } from '../Services/Requests';
 
 function PatientsProvider({ children }) {
   const [patients, setPatients] = useState([]);
+
+  const getPatients = async () => {
+    const retorno = await getAllPatients();
+    setPatients(retorno);
+  };
+
+  const deletePatient = async (id) => {
+    await deletePatientById(id);
+    const retorno = await getAllPatients();
+    setPatients(retorno);
+  };
+
+  const createPatient = async (body) => {
+    await addPatient(body);
+    const retorno = await getAllPatients();
+    setPatients(retorno);
+  };
+
+  const updatePatient = async (body, id) => {
+    await updatePatientById(body, id);
+    const retorno = await getAllPatients();
+    setPatients(retorno);
+  };
+
+  const getById = async (id, callback) => {
+    const retorno = await getPatientById(id);
+    callback(retorno);
+  };
 
   const memo = useMemo(() => {
     const values = {
       patients,
       setPatients,
+      getPatients,
+      getById,
+      createPatient,
+      updatePatient,
+      deletePatient,
     };
     return values;
-  }, []);
+  }, [patients]);
 
   return (
     <PatientsContext.Provider value={ memo }>
