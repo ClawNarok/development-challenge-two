01e254097033fb2f0704f03669e7721c513cb83a
ClawNarok <arthurvgf@gmail.com>
2022-11-27 23:25:59 -0300

    update CDataGrid.jsx

--- frontend/src/Components/CDataGrid.jsx
+++ frontend/src/Components/CDataGrid.jsx
@@ -1,47 +1,39 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { DataGrid } from '@mui/x-data-grid';
-import { Box } from '@mui/system';
-import CButton from './CButton';
-import icons from '../Helpers/Icons';
+import PatientsContext from '../Context/PatientsContext';
 
-function CDataGrid() {
-  const [idSelected, setIdSelected] = useState('');
+function CDataGrid({ callback }) {
+  const { patients } = useContext(PatientsContext);
   const columnName = [
-    { field: 'name', headerName: 'Nome', width: 210 },
+    { field: 'firstName', headerName: 'Nome', width: 210 },
+    { field: 'lastName', headerName: 'Sobrenome', width: 210 },
     { field: 'birthDate', headerName: 'Data de nascimento', width: 130 },
     { field: 'email', headerName: 'Email', width: 150 },
     { field: 'addressName', headerName: 'Endereço', width: 210 },
     { field: 'addressNumber', headerName: 'Número', width: 70 },
+    { field: 'complement', headerName: 'Complemento', width: 90 },
     { field: 'city', headerName: 'Cidade', width: 130 },
     { field: 'state', headerName: 'Estado', width: 70 },
-    { field: 'postal', headerName: 'CEP', width: 90 },
+    { field: 'postal', headerName: 'CEP', width: 100 },
   ];
-  const rows = [];
+
+  // const rows = [];
   const COUNTROWS = 5;
   return (
-    <Box style={ { height: 400, width: '100%' } }>
-      <CButton
-        txtButton="Editar paciente"
-        callback={ () => console.log(`Editar id=${idSelected}`) }
-        disabled={ !idSelected }
-        icon={ icons.Edit }
-      />
-      <CButton
-        txtButton="Deletar paciente"
-        callback={ () => console.log(`Deletar id=${idSelected}`) }
-        disabled={ !idSelected }
-        icon={ icons.Delete }
-      />
-      <DataGrid
-        columns={ columnName }
-        rows={ rows }
-        pageSize={ 5 }
-        rowsPerPageOptions={ [COUNTROWS] }
-        onCellClick={ (cell) => setIdSelected(cell.id) }
-        hideFooterSelectedRowCount
-      />
-    </Box>
+    <DataGrid
+      columns={ columnName }
+      rows={ patients }
+      pageSize={ 5 }
+      rowsPerPageOptions={ [COUNTROWS] }
+      onCellClick={ (cell) => callback(cell.id) }
+      hideFooterSelectedRowCount
+    />
   );
 }
+
+CDataGrid.propTypes = {
+  callback: PropTypes.func.isRequired,
+};
 
 export default CDataGrid;
