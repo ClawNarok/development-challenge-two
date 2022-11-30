16ccd2be366c528e42f95bed8b1b387941974773
ClawNarok <arthurvgf@gmail.com>
2022-11-30 08:42:38 -0300

    chore: delete patientRoutes.js

--- backend/src/Routes/patientRoutes.js
+++
@@ -1,9 +0,0 @@
-const patientRoute = {
-  GET: (e) => console.log('Get: ', JSON.stringify(e)),
-  POST: (e) => console.log('Post: ', JSON.stringify(e)),
-  PUT: (e) => console.log('Put: ', JSON.stringify(e)),
-  DELETE: (e) => console.log('Delete: ', JSON.stringify(e)),
-};
-
-// module.exports = patientRoute;
-export default patientRoute;
